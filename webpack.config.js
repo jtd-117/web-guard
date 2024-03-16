151cc51941a61ebddf57a3e26ca87afcc2583ba1
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-03-16 13:34:39 +1100

    Install HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode:'development',
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Web Guard',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
