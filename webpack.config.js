c0de30c569345a99ddbe9a2091dbfd2266ebe863
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-03-26 13:12:37 +1100

    Install Devtool for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     entry: {
         main: "./src/index.js",
     },
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: "Web Guard",
